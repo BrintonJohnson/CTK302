bd74fabdc730aa06e24187a4206b647ba56127bb
BrintonJohnson <59932628+BrintonJohnson@users.noreply.github.com>
2020-11-18 13:47:54 -0600

    gtest 4

--- p5/Unit4/gyro/sketch.js
+++ p5/Unit4/gyro/sketch.js
@@ -27,7 +27,7 @@ function draw() {
   // takes your variable and maps it from range 1 to range 2
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
   xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, 0, 180, 0, height);
+  yPosition = map(beta, 0, -1000, 0, height);
 
   push(); // before you use translate, rotate, or scale commands, push and then pop after
 
