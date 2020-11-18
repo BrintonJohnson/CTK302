06fd6743006978f9b1eab49cbf2aea7a3d8aa804
BrintonJohnson <59932628+BrintonJohnson@users.noreply.github.com>
2020-11-18 14:15:42 -0600

    gtest 5

--- p5/Unit4/gyro/sketch.js
+++ p5/Unit4/gyro/sketch.js
@@ -26,14 +26,15 @@ function draw() {
   // the map command !!!!
   // takes your variable and maps it from range 1 to range 2
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
-  xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, 0, -1000, 0, height);
+  // xPosition = map(gamma, -60, 60, 0, width);
+  xPosition = map(alpha, 160, 200, width, 0);
+  yPosition = map(beta, 0, 90, 0, height/2);
 
   push(); // before you use translate, rotate, or scale commands, push and then pop after
 
   translate(xPosition, yPosition); // move everything over by x, y
 
-  rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
+  // rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
 
   image(ghostImage, 0, 0, 400, 400);
   // rect(0, 0, 100, 100) ;
@@ -41,20 +42,20 @@ function draw() {
 
 
   // Text commands that display debugging data
-  // textAlign(LEFT);
-  // textSize(20);
-  // fill('black');
-  // text("orientation data:", 25, 25);
-  // textSize(15);
-  // text("alpha: " + alpha, 25, 50);
-  // text("beta: " + beta, 25, 70);
-  // text("gamma: " + gamma, 25, 90);
-  // textSize(20);
-  // text("acceleration data:", 25, 125);
-  // textSize(15);
-  // text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  // text("y = " + y.toFixed(2), 25, 170);
-  // text("z = " + z.toFixed(4), 25, 190);
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);
 
   // Text that makes CTK type in the background
   fill('white');
