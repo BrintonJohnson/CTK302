8d9ed9c807d4cd0c5c2d40f743a91ff11e9c51e6
BrintonJohnson <59932628+BrintonJohnson@users.noreply.github.com>
2020-11-19 13:12:45 -0600

    gyrocollector resize

--- p5/Unit4/gyrocollector/sketch.js
+++ p5/Unit4/gyrocollector/sketch.js
@@ -60,7 +60,7 @@ function draw() {
   // image(bunnyImage, 0, 0, 500, 500);
   // fill('green');
   // ellipse(0, 0, 80, 80);
-  image(mouse, 0, 0, 215, 256);
+  image(mouse, 0, 0, 107, 128);
   pop();
 
 
@@ -154,7 +154,7 @@ function Car() {
     // ellipse(this.pos.x - 50, this.pos.y, 50, 50);
     // ellipse(this.pos.x + 50, this.pos.y, 50, 50);
     // rect(this.pos.x + 17, this.pos.y - 30, 80, 60) ;
-    image(cheese, this.pos.x, this.pos.y, 101, 81);
+    image(cheese, this.pos.x, this.pos.y, 50, 40);
   }
 
   this.drive = function() {
